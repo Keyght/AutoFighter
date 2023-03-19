f1ef8d74e226477b436085c3956da55852a76e79
Nikita <nikita_novakov@mail.ru>
2023-03-19 14:34:14 +0300

    Crete project, add resources, start scripting

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private GameObject _boss;
+    
+    
+}
